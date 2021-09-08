c10fc1d4cd16933b55e392811fe23edae8a0b477
Jona Brown <66106310+SorviusN@users.noreply.github.com>
2021-09-07 20:37:30 -0700

    Finishing touches

--- components/table.js
+++ components/table.js
@@ -1,58 +1,57 @@
-import React from "react";
-import Hours from "./data.js";
+import React, {useState} from "react";
 
-function Table()
-{
-    const sales = props.stores.map(store => store.hourlySales)
-    let dayTotal = 0;
-    sales.forEach(storeArr => dayTotal += storeArr.reduce((a, b) => a + b, 0));
+function Table(props) {
 
-    return (
-        <>
-            {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link> */}
+  const sales = props.stores.map(store => store.hourlySales)
+  let dayTotal = 0;
+  sales.forEach(storeArr => dayTotal += storeArr.reduce((a,b) => a + b, 0)) 
 
-            <div id="storeTable">
-                <table class="rounded-lg border-2 border-opacity-100 border-green-400">
-                    <tr>
-                    <th>Location</th>
-                    {props.hours.map((hours, idx) => 
-                    <th key={idx} class="rounded-lg border-2 border-opacity-100 border-green-400">{hours}</th>
-                    )}
-                    <th class="rounded-lg border-2 border-opacity-100 border-green-400">Totals</th>
-                    </tr>
+  return (
+    <>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
 
-                    {
-                        props.stores.map((store, idx) =>
-                        <tr>
-                        <td key={store.Id} class="rounded-lg border-2 border-opacity-100 border-green-400">{store.location}<button class="trashIcon" value={store.Id} onClick={props.deleteStore}><i class="material-icons">delete</i></button></td>
-                        {
-                            sales[idx].map( sales =>
-                                <td class="rounded-lg border-2 border-opacity-100 border-green-400"></td>
-                            )
-                        }
-                        <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales[idx].reduce((a, b) => a + b, 0)}</td>
-                        </tr>
-                        )
-                    }
-
-                    <tr>
-                        <th class="rounded-lg border-2 border-opacity-100 border-green-400">Totals</th>
-                        {
-                            props.hours.map((time, idx) => {
-                                let hourTotal = 0;
-                                sales.forEach(store => {
-                                    hourTotal += store[idx];
-                                })
-                                return <td class="rounded-lg border-opacity-100 border-green-400">{hourTotal}</td> 
-                            })
-                        }
+      <div id="storeTable">
+          <table class="rounded-lg border-2 border-opacity-100 border-green-400 bg-green-100">
+            <tr>
+              <th>Location</th>
+                {props.hours.map((hours, idx) =>
+                <th key={idx} class="rounded-lg border-2 border-opacity-100 border-green-400">{hours}</th>
+                )}
+              <th class="rounded-lg border-2 border-opacity-100 border-green-400">Totals</th>
+            </tr>
 
 
+            {
+              props.stores.map((store, idx) =>
+              <tr>
+              <td key={store.Id} class="rounded-lg border-2 border-opacity-100 border-green-400" >{store.location}<button class="trashIcon" value={store.Id} onClick={props.deleteStore}><i class="material-icons">delete</i></button></td>
+              {
+                sales[idx].map( sales => 
+                  <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales}</td>
+                  )
+              }
+              <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales[idx].reduce(( a, b ) => a + b, 0)}</td>
+              </tr>
+              )
+            }
 
-                    </tr>
-                </table>
-            </div>
-        </>
+            <tr>
+              <th class="rounded-lg border-2 border-opacity-100 border-green-400">Totals</th>
+              {
+                props.hours.map((time, idx) => {
+                  let hourTotal = 0;
+                  sales.forEach(store => {
+                    hourTotal += store[idx];
+                  })
+                  return <td class="rounded-lg border-2 border-opacity-100 border-green-400">{hourTotal}</td>
+                })
+              }
+              <td class="rounded-lg border-2 border-opacity-100 border-green-400">{dayTotal}</td>
+            </tr>
+
+          </table>
+        </div>
+      </>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,59 @@
 import Footer from '../components/footer.js'
 import Header from '../components/header.js'
-import Form from '../components/form.js'
+import Main from '../components/main.js'
 import Head from '../components/head.js'
-import { useState } from 'react'
+import Link from "next/link"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Home() {
 
   const [store, setStore] = useState({});
+  const [stores, setStores] = useState([]);
+ 
+  const hourly_sales = [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36];
+  const hourFlux = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6 ]
 
   const addStore = (newStore) => {
     setStore(newStore);
+    setStores([...stores, newStore]);
+    console.log(stores);
   }
+
+  //process.env.COOKIES_STAND_API
+  const getStores = async () => {
+    const response = await axios.get("https://salmon-cookie-api.azurewebsites.net/api/Store");
+    const storesList = response.data;
+    setStores(storesList)
+  }
+
+  const deleteStore = async (e) => {
+    e.preventDefault();
+    id = e.target.value;
+    const response = await axios.delete(`https://salmon-cookie-api.azurewebsites.net/api/Store/${id}`);
+    console.log(response);
+  }
+
+  // Allows you to just run once, in which you call getstores. Similar to Update method in Unity.
+  useEffect(() =>{
+    getStores();
+  },[]);
 
   return (
     <>
       <Head/>
       <Header/>
-      <div>
-        <Form onSubmit={addStore}/>
-        <pre>{JSON.stringify(store, undefined, 2)}</pre>
-      </div>
-      <Footer/>
+      <Link href="/overview"><a>Overview page</a></Link>
+      <div id="mainDiv">
+        <Main addStore={addStore} deleteStore={deleteStore} stores={stores}/>
+        {/* <pre>{JSON.stringify(store, undefined, 2)}</pre> */}
+      </div >
+      <Footer stores={stores}/>
     </>
   )
 }
 
 export default Home
+
+
+
