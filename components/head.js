be3a98a81ba7758b7868d1173610f1a9328fa41d
Jona Brown <66106310+SorviusN@users.noreply.github.com>
2021-10-21 09:46:17 -0700

    Update head.js

--- components/head.js
+++ components/head.js
@@ -4,9 +4,10 @@ import Link from "next/link"
 function Head() {
     return (
         <>
-        <title>Cookies Stand Admin Page</title>
+        <title>Cookies Stand Admin Page - Under construction</title>
         <div class="nav-container">
             <Link href="/"><a>Welcome!</a></Link>
+            <Link href="/"><a>Additional Link</a></Link>
         </div>
         </>
     )
