c45fcbfe81ab3ff5384b4ca12c31862e7531c174
Jona Brown <66106310+SorviusN@users.noreply.github.com>
2021-11-26 20:19:06 -0800

    Trivial Changes

--- components/table.js
+++ components/table.js
@@ -1,15 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 
 function Table(props) {
 
   const sales = props.stores.map(store => store.hourlySales)
   let dayTotal = 0;
+  let nightTotal = 0;
+
   sales.forEach(storeArr => dayTotal += storeArr.reduce((a,b) => a + b, 0)) 
 
   return (
     <>
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
-
       <div id="storeTable">
           <table class="rounded-lg border-2 border-opacity-100 border-green-400 bg-green-100">
             <tr>
@@ -19,18 +20,16 @@ function Table(props) {
                 )}
               <th class="rounded-lg border-2 border-opacity-100 border-green-400">Totals</th>
             </tr>
-
-
             {
               props.stores.map((store, idx) =>
               <tr>
-              <td key={store.Id} class="rounded-lg border-2 border-opacity-100 border-green-400" >{store.location}<button class="trashIcon" value={store.Id} onClick={props.deleteStore}><i class="material-icons">delete</i></button></td>
-              {
-                sales[idx].map( sales => 
-                  <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales}</td>
-                  )
-              }
-              <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales[idx].reduce(( a, b ) => a + b, 0)}</td>
+                <td key={store.Id} class="rounded-lg border-2 border-opacity-100 border-green-400" >{store.location}<button class="trashIcon" value={store.Id} onClick={props.deleteStore}><i class="material-icons">delete</i></button></td>
+                {
+                  sales[idx].map( sales => 
+                    <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales}</td>
+                    )
+                }
+                <td class="rounded-lg border-2 border-opacity-100 border-green-400">{sales[idx].reduce(( a, b ) => a + b, 0)}</td>
               </tr>
               )
             }
@@ -48,7 +47,6 @@ function Table(props) {
               }
               <td class="rounded-lg border-2 border-opacity-100 border-green-400">{dayTotal}</td>
             </tr>
-
           </table>
         </div>
       </>
